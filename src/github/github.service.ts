f1018d9f84f10633090bc7b2c158cf6802eb09a9
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-05-11 11:10:36 -0300

    validando retorno de campos do GITHUB

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, HttpService } from '@nestjs/common';
 
-
 @Injectable()
 export class GithubService {
   constructor(private httpService: HttpService) {}
@@ -8,6 +7,24 @@ export class GithubService {
   findByUsername(username: string) {
     const uri = process.env.GITHUB_API;
     const url = `${uri}/users/${username}`;
-    return this.httpService.get(url).toPromise();
+    return this.httpService
+      .get(url)
+      .toPromise()
+      .then(res => {
+        const result = res.data;
+        const campos = {
+          login: result.login,
+          id: result.id,
+          nomdeid: result.node_id,
+          name: result.name,
+          htmlurl: result.html_url,
+          reposurl: result.repos_url,
+          updatedat: result.updated_at,
+          createdat: result.created_at,
+          email: result.email,
+        };
+
+        return campos;
+      });
   }
 }
