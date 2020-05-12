25b073bae1537b9e152e4b42053c69b6dc0e50fd
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-05-12 10:07:33 -0300

    correção de erro

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 
-export const UserSchema = new mongoose.UserSchema({
+export const UserSchema = new mongoose.Schema({
   login: String,
   id: String,
   nodeid: String,
