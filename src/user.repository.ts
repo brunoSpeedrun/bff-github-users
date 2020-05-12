94244f96559a946108cc21e5a5d26ca0725adccb
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-05-12 10:25:29 -0300

    deletados por correção e duplicidade

--- src/user.repository.ts
+++
@@ -1,6 +0,0 @@
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-
-export class UserRepository {
-  constructor(@InjectModel('User') private productModel: Model<User>){}
-}
