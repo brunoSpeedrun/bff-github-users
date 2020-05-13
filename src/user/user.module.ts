7a0889e32b42d2ed47ebc25ff4f1155163c7df3d
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-05-13 17:29:30 -0300

    Usando a chamada do github e fazendo o test UNI

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { UserSchema } from './schemas/user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserService } from './user.service';
+import { UserRepository } from './user.repository';
+import { GithubService } from '../github/github.service';
 
 @Module({
   imports: [
+    HttpModule,
+
     MongooseModule.forFeature([
       { name: 'User', schema: UserSchema, collection: 'users' },
     ]),
   ],
+  providers: [UserService, UserRepository, GithubService],
 })
 export class UserModule {}
